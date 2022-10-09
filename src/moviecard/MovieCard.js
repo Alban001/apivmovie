64b0d42fbf26b92225a63ce1f70526143ac9927f
‘Alban001’ <eliasalban03@gmail.com>
2022-10-09 12:23:24 -0300

    arreglando responsividad

--- src/moviecard/MovieCard.js
+++ src/moviecard/MovieCard.js
@@ -3,7 +3,7 @@ import './moviecard.css'
 const MovieCard = ({poster_path, title}) => {
   return (
     <div className='moviecard'>
-      <img src={'https://image.tmdb.org/t/p/w500/' + poster_path} alt="Movies" width="300" height="400" />
+      <img className='moviecard__img' src={'https://image.tmdb.org/t/p/w500/' + poster_path} alt="Movies"  />
       <h3>{title}</h3>
       </div>
   )
